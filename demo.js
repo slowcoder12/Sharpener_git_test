6aac28d01f656925c0637055227f59e75dfcd258
slowcoder12 <132445668+slowcoder12@users.noreply.github.com>
2023-07-24 16:12:55 +0530

    added list items color and made it bold

--- demo.js
+++ demo.js
@@ -5,5 +5,11 @@ const header = document.getElementById("header-title");
         header.style.border = "2px solid black";
 
 const addTitle = document.getElementById("add-title");
-        addTitle.style.fontWeight = "bold"
-        addTitle.style.color = "green"
+        addTitle.style.fontWeight = "bold";
+        addTitle.style.color = "green";
+
+let thirdListItem = (document.getElementById("third-item"));
+thirdListItem.style.backgroundColor = 'green';
+
+let listItems = document.getElementById('items');
+listItems.style.fontWeight = 'bold'
