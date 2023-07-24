19fae7d63e7e553b4ffadf89617b52e99f54b6ec
slowcoder12 <132445668+slowcoder12@users.noreply.github.com>
2023-07-24 15:23:09 +0530

    added html to test few selectors

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Sharpener_git_test
+
+created demo.js and recreated test.txt
--- demo.js
+++ demo.js
@@ -1,1 +1,9 @@
-console.log("Hello git test")
+console.log("Hello")
+document.title = "DOM Practice";
+
+const header = document.getElementById("header-title");
+        header.style.border = "2px solid black";
+
+const addTitle = document.getElementById("add-title");
+        addTitle.style.fontWeight = "bold"
+        addTitle.style.color = "green"
