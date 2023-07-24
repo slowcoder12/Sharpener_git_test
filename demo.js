a69c78e60efd7b7bfd1e71a5f8462cd111d1bfd5
slowcoder12 <132445668+slowcoder12@users.noreply.github.com>
2023-07-24 21:04:33 +0530

    tested queryselectors

--- demo.js
+++ demo.js
@@ -8,11 +8,24 @@ const addTitle = document.getElementById("add-title");
         addTitle.style.fontWeight = "bold";
         addTitle.style.color = "green";
 
-let thirdListItem = (document.getElementById("third-item"));
-thirdListItem.style.backgroundColor = 'green';
+// let thirdListItem = (document.getElementById("third-item"));
+// thirdListItem.style.backgroundColor = 'green';
 
 let listItems = document.getElementById('items');
 listItems.style.fontWeight = 'bold'
 
 let li = document.getElementsByTagName("li")[4];
 li.style.color = 'blue';
+
+// let secondItem= document.querySelector('#items li:nth-child(2)');
+// secondItem.style.backgroundColor ='green';
+
+//document.querySelector('#third-item').style.display = 'none';
+
+var secondItem = document.querySelectorAll('#items li')[1];
+    secondItem.style.color = 'green';
+
+    var oddItems = document.querySelectorAll('#items li:nth-child(odd)');
+    for (var i = 0; i < oddItems.length; i++) {
+      oddItems[i].style.backgroundColor = 'green';
+    }
