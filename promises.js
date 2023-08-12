ca39d1d4a15b6cbdda6b4207c56efd1fcab6db11
slowcoder12 <132445668+slowcoder12@users.noreply.github.com>
2023-08-12 18:26:12 +0530

    promiseall task

---
+++ promises.js
@@ -0,0 +1,49 @@
+const posts = [];
+let lastActivity;
+
+function updateLastUserActivity() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      lastActivity = new Date().toLocaleTimeString();
+      console.log(, lastActivity);
+      resolve();
+    }, 1000);
+  });
+}
+
+function createPost(post) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      posts.push(post);
+      console.log( post);
+      resolve();
+    });
+  });
+}
+
+function deletePost() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const deletedPost = posts.pop();
+      if (deletedPost) {
+        console.log(deletedPost);
+        resolve();
+      } else {
+        reject('empty.');
+      }
+    });
+  });
+}
+
+function updateLastUserActivityTime() {
+  return Promise.all([createPost('hello'), updateLastUserActivity()])
+    .then(() => deletePost())
+    .then(() => {
+      console.log(posts);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+updateLastUserActivityTime();
