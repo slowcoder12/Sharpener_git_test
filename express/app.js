c6c2372eb2d4539322d9faa146e09021da7eb3d0
slowcoder12 <132445668+slowcoder12@users.noreply.github.com>
2023-09-06 21:06:45 +0530

    filtering routes

--- express/app.js
+++ express/app.js
@@ -2,26 +2,22 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop")
 
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req,res,next)=>{
-    res.send('<html><body><form action = "/product" method = "POST"><label>product name</label><input type="text" name = "title"><label>Quantity</label><input type = "number" name = "size"><button type= "submit">Add Product</button></form></body></html>')
-});
+app.use("/admin",adminRouter);
+app.use("/shop",shopRouter);
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-  
-    res.redirect('/');
+app.use((req,res,next)=>{
+    res.status(404).send("<h1>error, page not found</h1>");
 
 });
 
-app.use('/',(req,res,next)=>{
-    console.log("main page");
-    res.send("<h1>main page</h1>")
 
-});
+
 
 
 
