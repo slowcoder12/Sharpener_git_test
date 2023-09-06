46a9d9330277f371cabf4028e5c5c29135c65656
slowcoder12 <132445668+slowcoder12@users.noreply.github.com>
2023-09-06 15:18:28 +0530

    routing - post method

--- express/app.js
+++ express/app.js
@@ -1,15 +1,28 @@
+
 const express = require('express');
 const app = express();
 
-app.use((req,res,next) => {
-    console.log("first middle");
-    next();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req,res,next)=>{
+    res.send('<html><body><form action = "/product" method = "POST"><label>product name</label><input type="text" name = "title"><label>Quantity</label><input type = "number" name = "size"><button type= "submit">Add Product</button></form></body></html>')
+});
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+  
+    res.redirect('/');
 
 });
 
-app.use((req,res,next)=>{
-    console.log("second middleware");
-    res.send('<h1>first express response</h1>');
+app.use('/',(req,res,next)=>{
+    console.log("main page");
+    res.send("<h1>main page</h1>")
+
 });
+
+
 
 app.listen(3000);
