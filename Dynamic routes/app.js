49b76fdca0091d545ddfa90c4e53ec8db2889cf7
slowcoder12 <132445668+slowcoder12@users.noreply.github.com>
2023-09-17 21:33:47 +0530

    sequelize intro

--- Dynamic routes/app.js
+++ Dynamic routes/app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
+
 
 const app = express();
 
@@ -21,5 +22,11 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+sequelize.sync().then(result=>{
+    app.listen(3000);
+   
+})
+.catch(err=>{
+    console.log(err);
+});
 
-app.listen(3000);
--- Dynamic routes/util/database.js
+++ Dynamic routes/util/database.js
@@ -1,10 +1,5 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node',
-    password: 'Practicecode'
-});
+const sequelize = new Sequelize('node','root','Practicecode',{dialect: 'mysql',host:'localhost'});
 
-module.exports = pool.promise();
+module.exports = sequelize;
